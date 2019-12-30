5c84be307391b8e6f374bb8f6204aede429633f4
masif088 <mokhamadasif@gmail.com>
2019-12-30 16:40:17 +0700

    My First App in React Native

---
+++ app/component/Note.js
@@ -0,0 +1,51 @@
+
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+
+export default class Note extends React.Component {
+  render(){
+    return (
+        <View key={this.props.keyval} style={styles.note}>
+            <Text style={styles.noteText}>{this.props.val.date} </Text>
+            <Text style={styles.noteText}>{this.props.val.note} </Text>
+
+            <TouchableOpacity onPress={this.props.deleteMethod} style={styles.noteDelete} >
+              <Text style={styles.noteDeleteText}> D</Text>
+            </TouchableOpacity>
+        </View>
+    );
+  }
+}
+
+const styles= StyleSheet.create({
+    note:{
+      position:'relative',
+      padding:20,
+      paddingRight:100,
+      borderBottomWidth:2,
+      borderBottomColor:'#121212'
+    },
+    noteText:{
+      paddingLeft:20,
+      borderLeftWidth:10,
+      borderLeftColor:'#121212',
+    },
+    noteDelete:{
+      position:'absolute',
+      justifyContent:'center',
+      alignItems:'center',
+      backgroundColor:'#000',
+      padding:10,
+      top:10,
+      bottom:10,
+      right:10
+    },
+    noteDeleteText:{
+      color:'white',
+    },
+});
